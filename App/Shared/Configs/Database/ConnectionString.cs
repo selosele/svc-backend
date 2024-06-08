8bbfe144f8350693f9bbb39b7b73e3178bbae6d6
selosele <osh3384@naver.com>
2024-06-08 14:53:29 +0900

    리팩토링

--- App/Shared/Configs/Database/ConnectionString.cs
+++ App/Shared/Configs/Database/ConnectionString.cs
@@ -1,7 +1,7 @@
 namespace svc.App.Shared.Configs.Database;
 
 /// <summary>
-/// appsettings.json 파일의 ConnectionStrings
+/// appsettings.json 파일의 ConnectionStrings 클래스
 /// </summary>
 public class ConnectionString
 {
