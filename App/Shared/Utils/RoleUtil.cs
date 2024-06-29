d05b8d4c30787c2faddd280e1b0b5661e6781a86
selosele <osh3384@naver.com>
2024-06-29 12:11:57 +0900

    리팩토링

--- App/Shared/Utils/RoleUtil.cs
+++ App/Shared/Utils/RoleUtil.cs
@@ -6,23 +6,18 @@ namespace svc.App.Shared.Utils;
 public class RoleUtil
 {
     /// <summary>
-    /// 권한 ID 접두사
-    /// </summary>
-    public const string prefix = "ROLE_";
-    
-    /// <summary>
     /// 시스템 관리자 권한
     /// </summary>
-    public const string systemAdmin = $"{prefix}SYSTEM_ADMIN";
+    public const string systemAdmin = "ROLE_SYSTEM_ADMIN";
 
     /// <summary>
     /// 게시판 관리자 권한
     /// </summary>
-    public const string boardAdmin = $"{prefix}BOARD_ADMIN";
+    public const string boardAdmin = "ROLE_BOARD_ADMIN";
 
     /// <summary>
     /// 직원 권한
     /// </summary>
-    public const string employee = $"{prefix}EMPLOYEE";
+    public const string employee = "ROLE_EMPLOYEE";
 
 }
