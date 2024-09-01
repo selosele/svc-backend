51562a0ab6f5bca0313d0a32f6223e7ec470ea15
selosele <osh3384@naver.com>
2024-09-01 17:33:55 +0900

    리팩토링

--- App/Human/Vacation/Models/DTO/VacationResponseDTO.cs
+++ App/Human/Vacation/Models/DTO/VacationResponseDTO.cs
@@ -44,7 +44,7 @@ public record VacationResponseDTO
     /// <summary>
     /// 휴가 사용일수
     /// </summary>
-    public int? VacationDiff { get; set; }
+    public int? VacationUseCount { get; set; }
 
     /// <summary>
     /// 삭제 여부
