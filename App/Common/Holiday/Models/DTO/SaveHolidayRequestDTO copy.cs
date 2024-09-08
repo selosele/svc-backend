ddeae52002276399eabb8a563bf9e28660208f54
selosele <osh3384@naver.com>
2024-09-08 16:56:27 +0900

    휴일 추가/수정 API 개발 기초 작업

---
+++ App/Common/Holiday/Models/DTO/SaveHolidayRequestDTO copy.cs
@@ -0,0 +1,50 @@
+using System.ComponentModel.DataAnnotations;
+using Svc.App.Shared.Models.DTO;
+
+namespace Svc.App.Common.Holiday.Models.DTO;
+
+/// <summary>
+/// 휴일 추가/수정 요청 DTO
+/// </summary>
+public record SaveHolidayRequestDTO : HttpRequestDTOBase
+{
+    #region Fields
+    /// <summary>
+    /// 일자
+    /// </summary>
+    [Required]
+    [MaxLength(8)]
+    public string? Ymd { get; set; }
+
+    /// <summary>
+    /// 연도
+    /// </summary>
+    [MaxLength(4)]
+    public string? YYYY { get; set; }
+
+    /// <summary>
+    /// 월
+    /// </summary>
+    [MaxLength(2)]
+    public string? MM { get; set; }
+
+    /// <summary>
+    /// 일
+    /// </summary>
+    [MaxLength(2)]
+    public string? DD { get; set; }
+
+    /// <summary>
+    /// 휴일명
+    /// </summary>
+    [MaxLength(30)]
+    public string? HolidayName { get; set; }
+
+    /// <summary>
+    /// 휴일 내용
+    /// </summary>
+    [MaxLength(100)]
+    public string? HolidayContent { get; set; }
+    #endregion
+
+}
