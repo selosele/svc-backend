60dc3367acff8c78c462404790ddccb9093c7203
selosele <osh3384@naver.com>
2024-09-16 17:34:37 +0900

    HTTP 상태값 유틸 클래스 생성

---
+++ App/Shared/Utils/HttpStatusUtil.cs
@@ -0,0 +1,20 @@
+namespace Svc.App.Shared.Utils;
+
+/// <summary>
+/// HTTP 상태값 유틸 클래스
+/// </summary>
+public class HttpStatusUtil
+{
+    #region Fields
+    /// <summary>
+    /// 실패
+    /// </summary>
+    public const int FAIL = 0;
+
+    /// <summary>
+    /// 성공
+    /// </summary>
+    public const int SUCCESS = 1;
+    #endregion
+
+}
